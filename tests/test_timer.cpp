38756119a8f875cb71b8f04a7a946c6db204bc60
Lineath <fran.montserrat.developer@gmail.com>
2025-10-13 20:58:54 +0200

    timer control fucntions tests added, passed

--- tests/test_timer.cpp
+++ tests/test_timer.cpp
@@ -19,7 +19,7 @@ namespace zenith {
 
 	// Verify initial elapsed time
 	TEST(TimerTest, InitialElapsedTimeIsZero) {
-		Timer timer(Minutes(5), Seconds(30));
+		Timer timer(Minutes(0), Seconds(10));
 		EXPECT_EQ(timer.GetStatus().elapsed, "00:00");
 	}
 
@@ -48,8 +48,94 @@ namespace zenith {
 		timer.Pause();
 		std::this_thread::sleep_for(std::chrono::seconds(2));
 		auto status = timer.GetStatus();
+		EXPECT_EQ(status.state, Timer::State::Paused);
 		EXPECT_EQ(status.remaining, "00:08");
 		EXPECT_EQ(status.elapsed, "00:02");
 	}
 
+	// Verify timer pauses only in running state
+	TEST(TimerTest, TimerPausesOnlyRunning) {
+		Timer timer(Minutes(0), Seconds(2));
+		timer.Start();
+		std::this_thread::sleep_for(std::chrono::seconds(3));
+		timer.Pause();
+		auto status = timer.GetStatus();
+		EXPECT_EQ(status.state, Timer::State::Ended);
+		EXPECT_EQ(status.remaining, "00:00");
+		EXPECT_EQ(status.elapsed, "00:02");
+		timer.Reset();
+		std::this_thread::sleep_for(std::chrono::seconds(1));
+		timer.Pause();
+		status = timer.GetStatus();
+		EXPECT_EQ(status.state, Timer::State::Stopped);
+		EXPECT_EQ(status.remaining, "00:02");
+		EXPECT_EQ(status.elapsed, "00:00");
+	}
+
+	// Verify time continue after resuming a pause
+	TEST(TimerTest, TimeContinueAfterResume) {
+		Timer timer(Minutes(0), Seconds(10));
+		timer.Start();
+		std::this_thread::sleep_for(std::chrono::seconds(1));
+		timer.Pause();
+		std::this_thread::sleep_for(std::chrono::seconds(1));
+		timer.Resume();
+		std::this_thread::sleep_for(std::chrono::seconds(1));
+		auto status = timer.GetStatus();
+		EXPECT_EQ(status.state, Timer::State::Running);
+		EXPECT_EQ(status.remaining, "00:08");
+		EXPECT_EQ(status.elapsed, "00:02");
+	}
+
+	// Verify timer resets to default
+	TEST(TimerTest, TimerReset) {
+		Timer timer(Minutes(0), Seconds(10));
+		timer.Start();
+		std::this_thread::sleep_for(std::chrono::seconds(2));
+		timer.Reset();
+		auto status = timer.GetStatus();
+		EXPECT_EQ(status.state, Timer::State::Stopped);
+		EXPECT_EQ(status.remaining, "00:10");
+		EXPECT_EQ(status.elapsed, "00:00");
+	}
+
+	// Verify timer resets after end
+	TEST(TimerTest, TimerResetAfterEnd) {
+		Timer timer(Minutes(0), Seconds(2));
+		timer.Start();
+		std::this_thread::sleep_for(std::chrono::seconds(3));
+		timer.Reset();
+		auto status = timer.GetStatus();
+		EXPECT_EQ(status.state, Timer::State::Stopped);
+		EXPECT_EQ(status.remaining, "00:02");
+		EXPECT_EQ(status.elapsed, "00:00");
+	}
+
+	// Verify timer ends after target time elapsed
+	TEST(TimerTest, TimerEnded) {
+		Timer timer(Minutes(0), Seconds(2));
+		timer.Start();
+		std::this_thread::sleep_for(std::chrono::seconds(3));
+		auto status = timer.GetStatus();
+		EXPECT_EQ(status.state, Timer::State::Ended);
+		EXPECT_EQ(status.remaining, "00:00");
+		EXPECT_EQ(status.elapsed, "00:02");
+	}
+
+	// Verify timer start again after ended
+	TEST(TimerTest, TimerStartAfterEnd) {
+		Timer timer(Minutes(0), Seconds(2));
+		timer.Start();
+		std::this_thread::sleep_for(std::chrono::seconds(3));
+		auto status = timer.GetStatus();
+		EXPECT_EQ(status.state, Timer::State::Ended);
+		EXPECT_EQ(status.remaining, "00:00");
+		EXPECT_EQ(status.elapsed, "00:02");
+		timer.Start();
+		std::this_thread::sleep_for(std::chrono::seconds(1));
+		status = timer.GetStatus();
+		EXPECT_EQ(status.state, Timer::State::Running);
+		EXPECT_EQ(status.remaining, "00:01");
+		EXPECT_EQ(status.elapsed, "00:01");
+	}
 }
