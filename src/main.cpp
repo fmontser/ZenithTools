84e67674feb397075586aa63f5b2716cf5a690cd
Lineath <fran.montserrat.developer@gmail.com>
2025-10-16 19:30:21 +0200

    added SFML, IMGUI and Imgui-SMFL adapter to project

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,34 @@
-#include "Timer.hpp"
-#include <thread>
+#include <SFML/Graphics.hpp>
+#include "imgui.h"
+#include "imgui-SFML.h"
 #include <iostream>
 
-using namespace zenith;
-using std::cout;
-
 int main(int argc, char* argv[]){
-	auto testTimer = Timer(Minutes(0), Seconds(10));
+	
+	//TODO Delete Imgui library test
+	sf::RenderWindow window(sf::VideoMode(800, 600), "ZenithTools");
+	window.setFramerateLimit(60);
+	ImGui::SFML::Init(window);
 
-	testTimer.Start();
+	sf::Clock deltaClock;
+	while (window.isOpen()) {
+		sf::Event event;
+		while (window.pollEvent(event)) {
+			ImGui::SFML::ProcessEvent(window, event);
+			if (event.type == sf::Event::Closed) {
+				window.close();
+			}
+		}
 
-	while(testTimer.GetStatus().state != Timer::State::Ended) {
+		ImGui::SFML::Update(window, deltaClock.restart());
 
-		std::this_thread::sleep_for(Seconds(2));
-		cout << testTimer.GetStatus().elapsed << "\n";
+		ImGui::ShowDemoWindow();
+
+		window.clear();
+		ImGui::SFML::Render(window);
+		window.display();
 	}
 
+	ImGui::SFML::Shutdown();
 	return 0;
 }
