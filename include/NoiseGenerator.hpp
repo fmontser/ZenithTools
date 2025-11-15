0c00e40d242cec2bbcb431dd05dce35536526163
Lineath <fran.montserrat.developer@gmail.com>
2025-11-15 18:38:20 +0100

    wrote bandfilter class structure

--- include/NoiseGenerator.hpp
+++ include/NoiseGenerator.hpp
@@ -9,28 +9,44 @@
 #include "SFML/Audio/SoundStream.hpp"
 #include <vector>
 #include <functional>
-#include <string>
+#include <array>
 
 namespace zenith {
 
 	class NoiseGenerator : public sf::SoundStream {
 		public:
-
-			enum class NoiseColor {
-				BROWN, PINK, WHITE, BLUE, VIOLET
+			enum class BandFilter {
+				_63HZ, _125HZ, _250HZ, _500HZ, _1KHZ, _2KHZ, _4KHZ, _8KHZ
 			};
-
-			NoiseGenerator();
 			
 			float volume;
 			bool muted;
-
+			
+			NoiseGenerator(BandFilter bandFrequency);
+			
 		private:
+			using FilterState = std::array<float, 4>;
+
+			enum class FilterIndex {
+				IN1, IN2, OUT1, OUT2
+			};
+
+			std::vector<sf::Int16> _buffer;
+			std::function<float (float, FilterState&)> _filter;
+			FilterState _fState;
+
 			bool onGetData(Chunk& data) override;
 			void onSeek(sf::Time timeOffset) override;
 			
 			void GenerateNoise();
+			static float BandFilter63Hz(float white, FilterState& fState);
+			static float BandFilter125Hz(float white, FilterState& fState);
+			static float BandFilter250Hz(float white, FilterState& fState);
+			static float BandFilter500Hz(float white, FilterState& fState);
+			static float BandFilter1KHz(float white, FilterState& fState);
+			static float BandFilter2KHz(float white, FilterState& fState);
+			static float BandFilter4KHz(float white, FilterState& fState);
+			static float BandFilter8KHz(float white, FilterState& fState);
 
-			std::vector<sf::Int16> _buffer;
 	};
 }
--- src/NoiseGenerator.cpp
+++ src/NoiseGenerator.cpp
@@ -13,16 +13,26 @@ constexpr uint SampleRate = 48000;
 const sf::Int16 MaxAmplitude =  32767;
 const sf::Int16 MinAmplitude = -32767;
 
-NoiseGenerator::NoiseGenerator() : sf::SoundStream() {
+NoiseGenerator::NoiseGenerator(BandFilter bandFrequency) : sf::SoundStream() {
 	volume = 50.0f;
 	muted = true;
 	initialize(2, SampleRate);
 	setVolume(volume);
+	switch (bandFrequency) {
+		case BandFilter::_63HZ:   _filter = BandFilter63Hz;   break;
+		case BandFilter::_125HZ:  _filter = BandFilter125Hz;  break;
+		case BandFilter::_250HZ:  _filter = BandFilter250Hz;  break;
+		case BandFilter::_500HZ:  _filter = BandFilter500Hz;  break;
+		case BandFilter::_1KHZ:   _filter = BandFilter1KHz;   break;
+		case BandFilter::_2KHZ:   _filter = BandFilter2KHz;   break;
+		case BandFilter::_4KHZ:   _filter = BandFilter4KHz;   break;
+		case BandFilter::_8KHZ:   _filter = BandFilter8KHz;   break;
+		default:                                              break;
+	}
 }
 
 bool NoiseGenerator::onGetData(Chunk &data) {
 	GenerateNoise();
-
 	data.samples = _buffer.data();
 	data.sampleCount = _buffer.size();
 	_buffer.clear();
@@ -46,4 +56,53 @@ void NoiseGenerator::GenerateNoise() {
 		_buffer[i * Channels] = normalized; // L
 		_buffer[i * Channels + 1] = normalized; // R
 	}
+}
+
+
+float NoiseGenerator::BandFilter63Hz(float white, FilterState& fState) {
+	// TODO
+	throw NotImplementedException();
+	return white;
+}
+
+float NoiseGenerator::BandFilter125Hz(float white, FilterState& fState) {
+	// TODO
+	throw NotImplementedException();
+	return white;
+}
+
+float NoiseGenerator::BandFilter250Hz(float white, FilterState& fState) {
+	// TODO
+	throw NotImplementedException();
+	return white;
+}
+
+float NoiseGenerator::BandFilter500Hz(float white, FilterState& fState) {
+	// TODO
+	throw NotImplementedException();
+	return white;
+}
+
+float NoiseGenerator::BandFilter1KHz(float white, FilterState& fState) {
+	// TODO
+	throw NotImplementedException();
+	return white;
+}
+
+float NoiseGenerator::BandFilter2KHz(float white, FilterState& fState) {
+	// TODO
+	throw NotImplementedException();
+	return white;
+}
+
+float NoiseGenerator::BandFilter4KHz(float white, FilterState& fState) {
+	// TODO
+	throw NotImplementedException();
+	return white;
+}
+
+float NoiseGenerator::BandFilter8KHz(float white, FilterState& fState) {
+	// TODO
+	throw NotImplementedException();
+	return white;
 }
